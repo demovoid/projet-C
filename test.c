9eaa810e7cf52e10ad6e591a156d410e758be258
Demo <tom.didier4@gmail.com>
2020-09-21 10:57:26 +0200

    temp

--- test.c
+++ test.c
@@ -1,9 +1,30 @@
 #include "./graph.h"
 
 int main(){
+	int val;
 	
 	//Test de création du graphe
-	//graph* monGraph = LoadGraphFromFile("./");
+	/*
+	
+	graph* monGraph = LoadGraphFromFile("./map.ini");
+	
+	*/
+	
+	for(int i = 0; i < NB_GROUND; i++){
+		val = GetLayerIDFromChar(s_groundChar[i]);
+		printf("Pour %c: %d\n", s_groundChar[i], val);
+	}
+	
+	/*
+		
+	*/
+	
+	//Test de libération du graphe
+	/*
+	
+	freeGraph(monGraph);
+	
+	*/
 	
 	return 0;
 }
