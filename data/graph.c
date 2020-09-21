3dd73c7d22ffc4d0cf1a12fc01aaa65ebf875745
Demo <tom.didier4@gmail.com>
2020-09-21 15:23:34 +0200

    code déplacé dans le .c
    parce que je suis con

---
+++ data/graph.c
@@ -0,0 +1,86 @@
+graph* LoadGraphFromFile(char* fichier){
+	
+	graph* monGraph = NULL;
+	FILE* monFichier = fopen(fichier,"r");
+	
+	if(!monFichier)
+		return NULL;
+	
+	int sizeX, sizeY;
+	fscanf("%d%d", &sizeX, &sizeY);
+	
+	if(sizeX <= 0 || sizeY <= 0){
+		fclose(monFichier);
+		return NULL;
+	}
+		
+	monGraph = malloc(sizeof(graph));
+	
+	monGraph->m_sizeX = sizeX;
+	monGraph->m_sizeY = sizeY;
+	
+	monGraph->m_data = malloc(sizeof(node*)*sizeY*sizeX);
+	
+	for(int i = 0; i < sizeX*sizeY; i++)
+		monGraph->m_data[i] = malloc(sizeof(node));
+	
+	char c;
+	
+	for(int i = 0; i < sizeY*sizeX; i++){
+		fscanf("%c",&c); //erreur si fichier non complet !!!
+		monGraph->m_data[i]->id = i;
+		printf("id: %d\n, monGraph->m_data[i]->id);
+		monGraph->m_data[i]->m_posX = i%sizeX;
+		monGraph->m_data[i]->m_posY = i/sizeX;
+		
+		switch(c){
+			case 'G':
+				monGraph->m_data[i]->m_layer = GRASS;
+				monGraph->m_data[i]->m_layerID = GRASS_ID;
+				break;
+			case 'F':
+				monGraph->m_data[i]->m_layer = FOREST;
+				monGraph->m_data[i]->m_layerID = FOREST_ID;
+				break;
+			case 'M':
+				monGraph->m_data[i]->m_layer = MOUNTAIN;
+				monGraph->m_data[i]->m_layerID = MOUNTAIN_ID;
+				break;
+			case 'C':
+				monGraph->m_data[i]->m_layer = CITY;
+				monGraph->m_data[i]->m_layerID = CITY_ID;
+				break;
+			case 'R':
+				monGraph->m_data[i]->m_layer = ROAD;
+				monGraph->m_data[i]->m_layerID = ROAD_ID;
+				break;
+			case 'W':
+				monGraph->m_data[i]->m_layer = WATER;
+				monGraph->m_data[i]->m_layerID = WATER_ID;
+				break;
+			default:
+				monGraph->m_data[i]->m_layer = NOTHING;
+				monGraph->m_data[i]->m_layerID = -1; //Signifie que l'objet n'a pas d'ID
+		}
+		monGraph->m_data[i]->m_neighbors[0] = monGraph->m_data[i]->m_posY ? &monGraph->m_data[i-sizeX] : NULL;
+		monGraph->m_data[i]->m_neighbors[1] = monGraph->m_data[i]->m_posX ? monGraph->m_data[i-1] : NULL;
+		monGraph->m_data[i]->m_neighbors[2] = (monGraph->m_data[i]->m_posX+1) < sizeX ? monGraph->m_data[i+1] : NULL;
+		monGraph->m_data[i]->m_neighbors[3] = ((monGraph->m_data[i]->m_posY+1)) < sizeY ? monGraph->m_data[i+sizeX] : NULL;
+		monGraph->m_data[i]->m_data = NULL;
+	}
+	
+	return graph;
+}
+
+void FreeGraph(graph* monGraph){
+	if(!monGraph || !monGraph->m_data)
+		return;
+	
+	for(int i = 0; i < sizeY*sizeX; i++){
+		if(m_data)
+			free(m_data); //peut causer des problèmes si m_data est d'un type défini par l'utilisateur
+		free(monGraph->m_data[i]);
+	}
+	free(monGraph->m_data);
+	free(monGraph);
+}
